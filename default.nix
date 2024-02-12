add2eeca53258bf1f47af47ba4d84f9848da0eac
MyyPo <nikirsmcgl@gmail.com>
2024-02-12 12:17:13 +0200

    fix: try fix meta

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ pkgs ?  import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
-  firmware = import ../src {};
-  config = ./.;
+  firmware = import ../src { };
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_left = firmware.zmk.override { board = "glove80_lh"; keymap = "./glove80.keymap"; kconfig = "./glove80.conf"; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "./glove80.keymap"; kconfig = "./glove80.conf"; };
 
-in firmware.combine_uf2 glove80_left glove80_right
+in
+firmware.combine_uf2 glove80_left glove80_right
