2035e2db4b88de134a79a9366f3331b62ae4a3ab
MyyPo <nikirsmcgl@gmail.com>
2024-02-12 12:25:13 +0200

    idk

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  firmware = import ../src { };
+  firmware = import ./src { };
 
   glove80_left = firmware.zmk.override { board = "glove80_lh"; keymap = "./glove80.keymap"; kconfig = "./glove80.conf"; };
   glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "./glove80.keymap"; kconfig = "./glove80.conf"; };
